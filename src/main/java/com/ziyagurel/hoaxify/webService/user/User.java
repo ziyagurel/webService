f37acc13f98e559e32b906414874fda1a99a6595
ziyagurel <ziyagurel55@hotmail.com>
2020-06-11 14:06:35 +0300

    User package created.Also the error in the pom.xml file has been solved

---
+++ src/main/java/com/ziyagurel/hoaxify/webService/user/User.java
@@ -0,0 +1,11 @@
+package com.ziyagurel.hoaxify.webService.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+	private String username;
+	private String displayName;
+	private String password;
+}
