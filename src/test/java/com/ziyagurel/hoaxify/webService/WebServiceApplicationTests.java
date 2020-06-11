406b766ad960e68348302dc2d2c056b625aa7b52
ziyagurel <ziyagurel55@hotmail.com>
2020-06-11 13:11:55 +0300

    Create new web Service for hoaxfy

---
+++ src/test/java/com/ziyagurel/hoaxify/webService/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziyagurel.hoaxify.webService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
