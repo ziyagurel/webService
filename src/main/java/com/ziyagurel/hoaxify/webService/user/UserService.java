d9e1e43353e9198bce14ab6328b0f2032cfe274c
ziyagurel <ziyagurel55@hotmail.com>
2020-06-13 12:22:56 +0300

    Spring Security added. User password has been encrypted.

--- src/main/java/com/ziyagurel/hoaxify/webService/user/UserService.java
+++ src/main/java/com/ziyagurel/hoaxify/webService/user/UserService.java
@@ -1,24 +1,28 @@
 package com.ziyagurel.hoaxify.webService.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 	
-	IUserRepository repository;
+	IUserRepository userRepository;
+	PasswordEncoder encoder;
 /**
  * Eğer constructor tek bir değer alıyor ise @Autowired edilmeye gerek yok.
  * @param repository
  */
 	@Autowired
-	public UserService(IUserRepository repository) {
-		super();
-		this.repository = repository;
+	public UserService(IUserRepository userRepository) {
+		this.userRepository = userRepository;
+		this.encoder = new BCryptPasswordEncoder();
 	}
 	
 	public void save(User user) {
-		repository.save(user);		
+		user.setPassword(this.encoder.encode(user.getPassword()));
+		userRepository.save(user);
 	}
 	
 }
